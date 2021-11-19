cf65b2e272e02eb150e82beebc9f91b5d2d8f4f0
Sergey <piolersa85@mail.ru>
2021-11-19 14:04:47 +0200

    34

--- The Rick and Morty API/RMTableViewCell.swift
+++ The Rick and Morty API/RMTableViewCell.swift
@@ -13,19 +13,24 @@ class RMTableViewCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var episodLabel: UILabel!
     
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
+  
     
     
-    
-    override func prepareForReuse() { // переиспользование и обнуления ячейки 
+    override func prepareForReuse() { // переиспользование и обнуления ячейки
         imageRM.image = nil
     }
     
-    func configure(with char: Result) {
+   func configure(with char: Result) {
+        
+       
+     
+        
         nameLabel.text = char.name
         episodLabel.text = char.species
         
-        
-        
+        indicator.stopAnimating()
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
             
             //  guard let stringURL = character.results.image else { return }
@@ -36,6 +41,7 @@ class RMTableViewCell: UITableViewCell {
             DispatchQueue.main.async {
                 
                 self?.imageRM.image = UIImage(data: imageData)
+                self?.indicator.stopAnimating()
             }
         }
     }
